c8d1407a65e2a3706b3a54c6b826da0db59b61de
Marshall Scorcio <mscorcio@swiftype.com>
2015-05-11 17:13:40 -0700

    No reason to block everything just to access a Queue

--- src/main/scala/net/lag/kestrel/QueueCollection.scala
+++ src/main/scala/net/lag/kestrel/QueueCollection.scala
@@ -18,8 +18,9 @@
 package net.lag.kestrel
 
 import java.io.File
-import java.util.concurrent.{CountDownLatch, ScheduledExecutorService}
+import java.util.concurrent.ScheduledExecutorService
 import scala.collection.mutable
+import collection.JavaConversions._
 import com.twitter.conversions.time._
 import com.twitter.logging.Logger
 import com.twitter.ostrich.stats.Stats
@@ -50,9 +51,9 @@ class QueueCollection(queueFolder: String, timer: Timer, journalSyncScheduler: S
     throw new InaccessibleQueuePath
   }
 
-  private val queues = new mutable.HashMap[String, PersistentQueue]
   private val fanout_queues = new mutable.HashMap[String, mutable.HashSet[String]]
   private val aliases = new mutable.HashMap[String, AliasedQueue]
+  private val queues : collection.mutable.Map[String, PersistentQueue] = new ConcurrentHashMap[String, PersistentQueue]
   @volatile private var shuttingDown = false
 
   @volatile private var queueBuilderMap = Map(queueBuilders.map { builder => (builder.name, builder) }: _*)
@@ -161,27 +162,32 @@ class QueueCollection(queueFolder: String, timer: Timer, journalSyncScheduler: S
    * Get a named queue, with control over whether non-existent queues are created.
    */
   def queue(name: String, create: Boolean, sessionDescription: SessionDescription): Option[PersistentQueue] =
+    if (shuttingDown) {
+      None
+    } else if (create) {
+      queues.get(name) orElse {
+        getOrCreateQueue(name, sessionDescription)
+      }
+    } else {
+      queues.get(name)
+    }
+
+  def getOrCreateQueue(name: String, sessionDescription: SessionDescription): Option[PersistentQueue] =
     synchronized {
-      if (shuttingDown) {
-        None
-      } else if (create) {
-        queues.get(name) orElse {
-          // only happens when creating a queue for the first time.
-          val q = if (name contains '+') {
-            val master = name.split('+')(0)
-            val fanoutQ = buildQueue(name, Some(master), path.getPath, sessionDescription)
-            fanout_queues.getOrElseUpdate(master, new mutable.HashSet[String]) += name
-            log.info("Fanout queue %s added to %s by %s", name, master, sessionDescription.getOrElse(unknown)())
-            fanoutQ
-          } else {
-            buildQueue(name, None, path.getPath, sessionDescription)
-          }
-          q.setup
-          queues(name) = q
-          Some(q)
+      queues.get(name) orElse {
+        // only happens when creating a queue for the first time.
+        val q = if (name contains '+') {
+          val master = name.split('+')(0)
+          val fanoutQ = buildQueue(name, Some(master), path.getPath, sessionDescription)
+          fanout_queues.getOrElseUpdate(master, new mutable.HashSet[String]) += name
+          log.info("Fanout queue %s added to %s by %s", name, master, sessionDescription.getOrElse(unknown)())
+          fanoutQ
+        } else {
+          buildQueue(name, None, path.getPath, sessionDescription)
         }
-      } else {
-        queues.get(name)
+        q.setup
+        queues(name) = q
+        Some(q)
       }
     }
 
