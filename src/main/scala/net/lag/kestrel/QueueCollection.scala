c5fbf9148d42f52d78cdec4183be75622d825f29
Marshall Scorcio <mscorcio@swiftype.com>
2015-05-12 16:55:04 -0700

    Cleanup

--- src/main/scala/net/lag/kestrel/QueueCollection.scala
+++ src/main/scala/net/lag/kestrel/QueueCollection.scala
@@ -172,7 +172,7 @@ class QueueCollection(queueFolder: String, timer: Timer, journalSyncScheduler: S
       queues.get(name)
     }
 
-  def getOrCreateQueue(name: String, sessionDescription: SessionDescription): Option[PersistentQueue] =
+  private def getOrCreateQueue(name: String, sessionDescription: SessionDescription): Option[PersistentQueue] =
     synchronized {
       queues.get(name) orElse {
         // only happens when creating a queue for the first time.
