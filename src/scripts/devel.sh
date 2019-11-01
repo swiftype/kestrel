e46d93b54c4b5291c7d579214091bfcbaac82f3f
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2019-11-01 17:44:52 -0400

    Don't use system dirs for devel

--- src/scripts/devel.sh
+++ src/scripts/devel.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 echo "Starting kestrel in development mode..."
 
-if ! [ -d /var/log/kestrel ] || ! [ -d /var/spool/kestrel ] ; then
-    echo "Creating some dirs as root"
-    sudo mkdir -p /var/log/kestrel /var/spool/kestrel
-    sudo chown $(whoami):  /var/log/kestrel /var/spool/kestrel
-fi
-
-echo "logs:   /var/log/kestrel"
-echo "queues: /var/spool/kestrel"
-
 # find jar no matter what the root dir name
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do
